a65671d534c0dc83353eb659e19bd9b594975555
Victoria Catterson <vic@cowlet.org>
2024-12-07 16:43:01 -0800

    Day 6 part 2, finally

--- day6/day6.py
+++ day6/day6.py
@@ -1,5 +1,4 @@
 import itertools
-
 guard = "^>v<"
 
 class Obstacle:
@@ -27,24 +26,25 @@ def make_blank_grid(lines):
                 grid[i][j] = "."
     return grid
 
-def trace_path(grid):
-    gi, gj = 0, 0
-    obstacles = []
+def find_items(grid, items):
+    locations = []
     for i, line in enumerate(grid):
         for j, ch in enumerate(line):
-            if ch in guard:
-                gi, gj = i, j
-            if ch == "#":
-                obstacles.append(Obstacle(i, j))
+            if ch in items:
+                locations.append([i, j])
+    return locations
 
-    # Did we really find the guard, or did we run out of board?
+def trace_path(grid):
+    locs = find_items(grid, guard)
+    if len(locs) != 1:
+        raise AttributeError(f"Wrong number of guards! {len(locs)}")
+    gi, gj = locs[0][0], locs[0][1]
     on_grid = True
-    if grid[gi][gj] not in guard:
-        print(f"*** Error: Could not find the guard at the start!! ***")
-        raise AttributeError("No guard")
-        on_grid = False
 
-    obstacles_hit = []
+    obstacles = find_items(grid, "#O")
+    obstacles = [Obstacle(loc[0], loc[1]) for loc in obstacles]
+
+    obstacles_hit = [] # Check for loops
     while on_grid:
         grid, gi, gj, on_grid, obs = step(gi, gj, grid)
         if obs:
@@ -76,7 +76,7 @@ def step(i, j, grid):
         #print(f"Guard exited the grid at ({new_i}, {new_j})")
         return grid, new_i, new_j, False, obs
 
-    if grid[new_i][new_j] == "#":
+    if grid[new_i][new_j] in "#O":
         obs = Obstacle(new_i, new_j, grid[i][j]) # Save for later
         # Stay in place and rotate
         idx = guard.index(grid[i][j]) + 1
@@ -95,55 +95,54 @@ def step(i, j, grid):
 with open("d6_input.txt", "r") as f:
     lines = f.readlines()
 
-grid = [list(line.strip()) for line in lines]
-#for row in grid:
-#    print("".join(row))
-
-
-# Reset the grid and try every position in the guard's path
+# Read the orig grid and try every position in the guard's path
 base_grid = [list(line.strip()) for line in lines]
-gi, gj = 0, 0
-for i, line in enumerate(base_grid):
-    for j, ch in enumerate(line):
-        if ch in guard:
-            gi, gj = i, j
+locs = find_items(base_grid, guard)
+if len(locs) != 1:
+    print(f"Wrong number of guards at the start! {len(locs)}")
+orig_gi, orig_gj = locs[0][0], locs[0][1]
+orig_gd = base_grid[orig_gi][orig_gj]
+gi, gj = orig_gi, orig_gj
 on_grid = True
+
 new_obs = []
 while on_grid:
     future_grid, new_i, new_j, new_on_grid, _ = step(gi, gj, base_grid)
-    #print(gi, gj, new_i, new_j, on_grid)
-    if new_on_grid and (new_i != gi or new_j != gj):
-        # If coords are the same, we've hit an obstacle already. Step again.
+    # If we step off the grid, can't place an obstacle there
+    # If both coords are the same, we've hit an obstacle already, so loop
+    # If we're at the original guard position, can't place
+    if new_on_grid and (new_i != gi or new_j != gj) and \
+            (new_i != orig_gi or new_j != orig_gj):
         #print(f"Testing object at ({new_i}, {new_j})")
         # Make a blank grid
         hypo_grid = make_blank_grid(lines)
-        # Place the guard at gi, gj in same heading
-        hypo_grid[gi][gj] = base_grid[gi][gj]
+        # Place the guard at original spot, so we know the object doesn't
+        # interfere with path to here
+        hypo_grid[orig_gi][orig_gj] = orig_gd
         # Place an obstacle at new_i, new_j
-        hypo_grid[new_i][new_j] = "#"
-        #for row in hypo_grid:
-        #    print("".join(row))
+        hypo_grid[new_i][new_j] = "O"
+
         # trace_path
         try:
-            _, _, _ = trace_path(hypo_grid)
+            new_grid, _, _ = trace_path(hypo_grid)
             #print(f"--> No loop, so drop obstacle ({new_i},{new_j})")
         except ValueError:
             # If we hit a loop exception, we completed the path!
             obs = Obstacle(new_i, new_j)
             #print(f"Valid new object at {obs}")
             new_obs.append(obs)
-            #for row in hypo_grid:
-            #    print("".join(row))
-    # Actually advance the guard on the base grid
+    # Actually advance the guard on the base grid, regardless of
+    # whether we tested an obstacle or not
     base_grid = future_grid
     gi, gj = new_i, new_j
     on_grid = new_on_grid
-    #print(f"*** After testing, true grid is ***")
-    #for row in base_grid:
-    #    print("".join(row))
 
-
-print(f"Possible objects to place: {len(new_obs)}")
-#for p in new_obs:
-#    print(p)
+# Filter out dups
+uniq = []
+for obs in new_obs:
+    # Only location matters now, not heading
+    match = [1 for it in uniq if it.i==obs.i and it.j==obs.j]
+    if len(match) == 0:
+        uniq.append(obs)
+print(f"Possible objects to place: {len(new_obs)}, uniques: {len(uniq)}")
 
